9cfa3718ab3dd609bddec16edaddd50311a324ff
AustinChiu <beyourgoodangel@gmail.com>
2025-06-09 04:16:41 +0800

    🐛fix router

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,10 @@ const router = createBrowserRouter([
     path: '/',
     element: <Home />
   }
-])
+],
+{
+    basename: '/zelda-totk-map' // <--- 加這行！對應到你的 vite.config.ts 中的 base
+})
 
 export default function App() {
   return (
